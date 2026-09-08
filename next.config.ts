190950d19fba11804b4036ffc7881dc0471ac1c7
Bagus <igoblin970@gmail.com>
2026-09-08 12:42:24 +0700

    Initial commit from Create Next App

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com", "p16-sign.tiktokcdn.com", "p19-sign.tiktokcdn.com"],
+  },
+};
+
+export default nextConfig;
